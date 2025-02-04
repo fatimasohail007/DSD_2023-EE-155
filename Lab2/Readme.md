84c92b4c6569c455a0acdd091167855f73f200c6
fatimasohail007 <fatimairfansohail1@gmail.com>
2025-02-04 01:53:03 -0800

    ''

---
+++ Lab2/Readme.md
@@ -0,0 +1,18 @@
+# DSD lab
+
+**Fatima Irfan Sohail**
+
+**2023-EE-155**
+
+### Simulation
+
+* 1 code file(.sv)
+* 1 constraint file(.xdc)
+
+### Report
+### Task 1
+Contains truth table for x and y.
+### Task 2
+Contains the maximum combinational delay.
+### task 3
+Contains the utilization report summary.
---
+++ Lab2/rtl/designCode.sv
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+////////////////////////////////////////////////////////////////////////////////// 
+// Engineer:Fatima Irfan Sohail
+// 
+// Module Name: Lab3
+// Target Devices:Nexys-A7-100T 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Lab3(output x , y ,
+input a,b,c
+ );
+
+ assign x = (a | b) ^ ~c;
+ assign y =(a|b) & (~(a&b)^(a|b));
+
+endmodule
